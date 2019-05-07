1437a008ccfb236c13dc0579409061e2433ac0fb
booellean <booelle.commissions@gmail.com>
2019-05-06 20:56:45 -0700

    feat: FINAL - Project utlizes Grunt, npm scripts and scss for final build

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $('#loginModalButton').click(function(){
+    $('#loginModal').modal('show');
+  });
+  $('#reserveModalButton').click(function(){
+    $('#reserveModal').modal('show');
+  });
+});
